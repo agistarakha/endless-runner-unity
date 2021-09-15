9cbfc06fa3845f776e3098859d7a6bb309fb9fdf
agistarakha <agistarakhasnc@gmail.com>
2021-09-15 14:49:02 +0700

    Base game

---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreController : MonoBehaviour
+{
+
+    [Header("Score Highlight")]
+    public int scoreHighlightRange;
+    public CharacterSoundController sound;
+
+    private int lastScoreHighlight = 0;
+    private int currentScore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastScoreHighlight = 0;
+        currentScore = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public float GetCurrentScore()
+    {
+        return currentScore;
+    }
+    public void IncreaseCurrentScore(int increment)
+    {
+        currentScore += increment;
+        if (currentScore - lastScoreHighlight > scoreHighlightRange)
+        {
+            sound.PlayScoreHighlight();
+            lastScoreHighlight += scoreHighlightRange;
+        }
+    }
+    public void FinishScoring()
+    {
+        if (currentScore > ScoreData.highScore)
+        {
+            ScoreData.highScore = currentScore;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Tugas Chapter 3 - Endless Runner
+## Nama : Agista Rakhasanca
+## ID Agate : 462
+### Note : Akses file standalone melalui ./Build
+
+## Fitur Tambahan
+
